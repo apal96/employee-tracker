9a8020107e32b974f4b3fa5f3b77dc958286c6f8
apal <alishapal96@gmail.com>
2021-07-25 21:20:31 -0700

    add employee table

--- schema.sql
+++ schema.sql
@@ -7,13 +7,20 @@ CREATE TABLE department(
     id INT NOT NULL PRIMARY KEY,
     name VARCHAR(30) NOT NULL -- will hold department name
 );
-CREATE TABLE employeeRole(
+CREATE TABLE role(
     id INT NOT NULL PRIMARY KEY,
     title VARCHAR(30) NOT NULL, -- will hold role title
     salary DECIMAL NOT NULL,
     department_id INT,
-    FOREIGN KEY (department_id)
-    REFERENCES department(id)
+    FOREIGN KEY (department_id) REFERENCES department(id)
     ON DELETE SET NULL 
-
+);
+CREATE TABLE employee(
+    id INT NOT NULL PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR (30) NOT NULL,
+    role_id INT,
+    FOREIGN KEY (role_id) REFERENCES role(id)
+    ON DELETE SET NULL
+    -- manager_id 
 );
