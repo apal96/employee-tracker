8bdc0776a255ea3de1fd7dc0fe5ffd4312bbeb99
apal <alishapal96@gmail.com>
2021-07-25 21:12:42 -0700

    add employeeRole table

--- schema.sql
+++ schema.sql
@@ -1,8 +1,19 @@
 DROP DATABASE IF EXISTS department_db;
-CREATE department_db;
+CREATE DATABASE department_db;
 
 USE department_db;
-CREATE TABLE departments(
+SELECT DATABASE ();
+CREATE TABLE department(
     id INT NOT NULL PRIMARY KEY,
-    name VARCHAR(30) -- will hold department name
+    name VARCHAR(30) NOT NULL -- will hold department name
+);
+CREATE TABLE employeeRole(
+    id INT NOT NULL PRIMARY KEY,
+    title VARCHAR(30) NOT NULL, -- will hold role title
+    salary DECIMAL NOT NULL,
+    department_id INT,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL 
+
 );
