31024ed4f655617f1cc3d68328c6fb5a3ed4221a
apal <alishapal96@gmail.com>
2021-07-25 20:49:42 -0700

    add department table

--- schema.sql
+++ schema.sql
@@ -1,1 +1,8 @@
-DROP DATABASE IF EXISTS
+DROP DATABASE IF EXISTS department_db;
+CREATE department_db;
+
+USE department_db;
+CREATE TABLE departments(
+    id INT NOT NULL PRIMARY KEY,
+    name VARCHAR(30) -- will hold department name
+);
